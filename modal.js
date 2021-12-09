db208a0d66df0c4d415e3bd439988d993574e2b8
Reimhinn <arnaud.bittler@gmail.com>
2021-12-09 14:44:30 +0100

    close x

--- modal.js
+++ modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeX = document.querySelector(".close");
 
-// launch modal event
+// launch/close modal event
+
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+closeX.addEventListener("click", closeModal);
+
+// launch/close modal form
+
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+function closeModal() {
+  modalbg.style.display = "none";
+}
